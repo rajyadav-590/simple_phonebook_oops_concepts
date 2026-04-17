ef0576b165c1d7bb1d09001227060d0d7fd98300
rajyadav-590 <rajyadavetah590@gmail.com>
2026-04-17 23:12:17 +0530

    update phonebook_miniproject.py file for universal version

--- phonebook_miniproject.py
+++ phonebook_miniproject.py
@@ -13,7 +13,51 @@ class Contacts:
     def display_all(cls):
         for contact in cls.phone_book:
             print(contact.display())
-contact_1 = Contacts("ram",9876543210)
-a = contact_1.display()
-contact_1.display_all()
-print(a)
+        print("--------------------------")
+
+    @classmethod
+    def search_no(cls, search_name):
+        for i in cls.phone_book:
+            if i.name.strip().lower() == search_name.strip().lower():
+                return i.phone_no
+        return f"contact name: {search_name} is not found......."
+
+    @staticmethod
+    def number_validation(number_input):
+        return number_input.isdigit() and 8 <= len(number_input) <= 10
+
+
+print("welcome to phonebook..")
+while True:
+    print("1. for adding contacts")
+    print("2. for displaying all contacts")
+    print("3. for searching contact by name")
+    print("4. Quit")
+    print("--------------------------------")
+
+    try:
+        n = int(input("Enter your choice...."))
+
+        if n == 1:
+            name_input = input("Enter the name: ")
+            number_input = input("Enter the number: ")
+
+            if Contacts.number_validation(number_input):
+                Contacts(name_input, number_input)
+                print("your contact is added successfully")
+            else:
+                print("Number must be between 8 to 10 digits")
+
+        elif n == 2:
+            Contacts.display_all()
+
+        elif n == 3:
+            search_name = input("Enter the contact name to be searched: ")
+            print(Contacts.search_no(search_name))
+            print("----------------------------")
+
+        elif n == 4:
+            break
+
+    except Exception:
+        print("your input is wrong")
